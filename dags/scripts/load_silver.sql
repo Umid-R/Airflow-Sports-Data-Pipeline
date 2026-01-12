e1a202bc74ae2995e4007f90ff73c06cc2c5a117
Umid-R <umidraxmatullayev96@gmail.com>
2026-01-12 20:57:23 +0900

    rebuilt silver layer

--- dags/scripts/load_silver.sql
+++ dags/scripts/load_silver.sql
@@ -10,20 +10,20 @@ BEGIN
     TRUNCATE TABLE silver.teams;
     TRUNCATE TABLE silver.matches;
     INSERT INTO silver.competitions (
-        competition_id,
-        competition_name,
-        competition_code,
-        competition_type,
-        competition_emblem
+        league_id ,           
+        league_name ,      
+        league_country,               
+        league_logo,                
+        league_flag
     )
     SELECT DISTINCT
-        c.cmt_id                             AS competition_id,
-        TRIM(c.name)                     AS competition_name,
-        c.code                           AS competition_code,
-        c.type                           AS competition_type,
-        c.emblem                         AS competition_emblem
+        c.id                             AS league_id,
+        TRIM(c.name)                     AS league_name ,
+        c.country                        AS league_country ,
+        c.logo                           AS league_logo,
+        c.flag                         AS league_flag
     FROM bronze.competitions c
-    WHERE c.cmt_id IS NOT NULL
+    WHERE c.id IS NOT NULL
     AND c.name IS NOT NULL;
 
 
@@ -32,53 +32,56 @@ BEGIN
         team_id,
         name,
         short_name,
-        team_abbr,
-        team_logo,
-        competition_id
+        country,
+        team_logo
+     
     )
     SELECT DISTINCT
         t.team_id                       AS team_id,
         TRIM(t.name)               AS name,
         t.short_name,
-        t.tla                      AS team_abbr,
-        t.crest                    AS team_logo,
-        t.competition_id
+        t.country                  AS country,
+        t.logo                    AS team_logo
+        
     FROM bronze.teams t
-    WHERE t.team_id IS NOT NULL
-    AND t.competition_id IS NOT NULL;
+    WHERE t.team_id IS NOT NULL;
 
 
 
     INSERT INTO silver.matches (
-        match_id,
-        competition_id,
-        season_id,
-        matchday,
-        stage,
-        utc_date,
-        status,
-        home_team_id,
-        away_team_id,
+        match_id,         
+        competition_id,               
+        season,               
+        round,     
+        date,             
+        status,               
+        home_team_id,      
+        away_team_id,     
         winner,
         full_time_home,
-        full_time_away,
-        half_time_home,
-        half_time_away
+        full_time_away,              
+        half_time_home,              
+        half_time_away 
     )
     SELECT DISTINCT
-        m.match_id                                AS match_id,
-        m.competition_id,
-        m.season_id,
-        m.matchday,
-        m.stage,
-        m.utc_date::timestamp,
-        m.status,
-        m.home_team_id,
+        m.match_id,         
+        m.competition_id,               
+        m.season,               
+        m.round,     
+        m.date,             
+        m.status,               
+        m.home_team_id,      
         m.away_team_id,
-        m.winner,
+
+        CASE
+            WHEN m.winner_home='true' THEN 'HOME'
+            WHEN m.winner_away='true' THEN 'AWAY'
+            ELSE 'DRAW'
+        END AS winner,
+
         m.full_time_home,
-        m.full_time_away,
-        m.half_time_home,
+        m.full_time_away,              
+        m.half_time_home,              
         m.half_time_away
     FROM bronze.matches m
     WHERE m.match_id IS NOT NULL;
